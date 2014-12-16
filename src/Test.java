595e152347f9371189feb684a765ed9436e3add3
soukwinder <sanjosinghmaddan@hotmail.com>
2014-12-16 15:36:33 +0100

    'ewa'

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+	
+	public void hello(){
+		
+		System.out.println("Hello World");
+
+	}
+		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test h = new Test();
+		h.hello();
+
+	}
+
+}
