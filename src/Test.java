40175379ef3674cb4fe7e6b38820c2bacdec1396
soukwinder <sanjosinghmaddan@hotmail.com>
2014-12-16 16:03:45 +0100

    'gefixt'

--- src/Test.java
+++ src/Test.java
@@ -3,15 +3,15 @@ public class Test {
 	
 	public void hello(){
 		
-		Saaystem.out.println("Hello World");
+		System.out.println("Hello World");
 
 	}
 		
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Testaa h = new Test();
-		h.hellaao();
+		Test h = new Test();
+		h.hello();
 
 	}
 
